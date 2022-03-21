6fb90e5534fe2ff6c6627f4b940123897b82ddf6
vynleran <pjahanlou@gmail.com>
2022-03-21 09:19:51 -0500

    Changed WaveView to be a circle

--- app/src/main/java/com/example/seizuredetectionapp/AppSettings.java
+++ app/src/main/java/com/example/seizuredetectionapp/AppSettings.java
@@ -5,6 +5,9 @@ import static com.example.seizuredetectionapp.Questionnaire.addedContacts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
