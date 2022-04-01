fed68cc2e86c044b16eedecc03c66094c987be67
vynleran <pjahanlou@gmail.com>
2022-04-01 16:20:17 -0500

    Put error handling and fixed issue with questionnaire medical

--- app/src/main/java/com/example/seizuredetectionapp/LocationPermission.java
+++ app/src/main/java/com/example/seizuredetectionapp/LocationPermission.java
@@ -6,9 +6,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,6 +21,8 @@ public class LocationPermission extends AppCompatActivity implements View.OnClic
     private static final int FINE_LOCATION_PERMISSION_CODE = 100;
     private static final int COARSE_LOCATION_PERMISSION_CODE = 101;
     private Button sureButton, notSureButton;
+    private SharedPreferences sharedPreferences;
+    private LocalSettings localSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,16 @@ public class LocationPermission extends AppCompatActivity implements View.OnClic
         // Adding click listener to the buttons
         sureButton.setOnClickListener(this);
         notSureButton.setOnClickListener(this);
+
+        sharedPreferences = getSharedPreferences(localSettings.PREFERENCES, Context.MODE_PRIVATE);
+        Log.d("boolQ", ""+sharedPreferences.getString("questionnaire bool", localSettings.getQuestionnaireComplete()));
+
+        // Logging the personal questionnaire data
+        Log.d("seizureTypes", ""+sharedPreferences.getStringSet("SeizureTypes", localSettings.getSeizureTypes()));
+        Log.d("firstSeizure", ""+sharedPreferences.getString("firstSeizure", ""));
+        Log.d("seizureFreq", ""+sharedPreferences.getString("seizureFrequencyPerMonth", ""));
+        Log.d("averageSeizure", ""+sharedPreferences.getString("seizureDuration", ""));
+        Log.d("longestSeizure", ""+sharedPreferences.getString("longestSeizure", ""));
     }
 
     @Override
--- app/src/main/java/com/example/seizuredetectionapp/QuestionnaireMedical.java
+++ app/src/main/java/com/example/seizuredetectionapp/QuestionnaireMedical.java
@@ -92,33 +92,7 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
         seizureTypeView.setAdapter(adapter);
 
         openDatePicker.setOnClickListener(this);
-
-        // Cool animation for the submit button
-        submitQuestionnaireMedical.setOnClickListener(v -> {
-            // Start the loading animation when the user tap the button
-            submitQuestionnaireMedical.startAnimation();
-
-            // Do your networking task or background work here.
-            final Handler handler = new Handler();
-            handler.postDelayed(() -> {
-                boolean isSuccessful = true;
-                saveQuestionnaireMedical();
-
-                // Choose a stop animation if your call was succesful or not
-                if (isSuccessful) {
-                    submitQuestionnaireMedical.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
-                        @Override
-                        public void onAnimationStopEnd() {
-                            Intent intent = new Intent(getBaseContext(), LocationPermission.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                            startActivity(intent);
-                        }
-                    });
-                } else {
-                    submitQuestionnaireMedical.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
-                }
-            }, 1000);
-        });
+        submitQuestionnaireMedical.setOnClickListener(this);
     }
 
     private String longestSeizureConvert(float value) {
@@ -132,7 +106,9 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
     }
 
     private String averageSeizureConvert(float value) {
-        if((int)value == 1){
+        if((int) value == 0){
+            return "0 sec";
+        } else if((int)value == 1){
             return "5 Sec";
         } else if((int)value == 2) {
             return "30 Sec";
@@ -157,7 +133,7 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.openDatePickerDialog: {
+            case R.id.openDatePickerDialog:
                 DatePickerDialog datePickerDialog = new DatePickerDialog(
                         this,
                         0,
@@ -168,7 +144,9 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
                 );
                 datePickerDialog.show();
                 break;
-            }
+            case R.id.submitQuestionnaireMedical:
+                saveQuestionnaireMedical();
+                break;
         }
     }
 
@@ -179,7 +157,25 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
         String averageSeizure = averageSeizureConvert(averageSeizureDurationSlider.getValues().get(0));
         String longestSeizure = longestSeizureConvert(longestSeizureSlider.getValues().get(0));
 
+        if (seizureTypes == null) {
+            seizureTypeView.requestFocus();
+            seizureTypeView.setError("A seizure type is required!");
+            return;
+        }
+
         if (seizureStartD == "") {
+            openDatePicker.requestFocus();
+            openDatePicker.setError("A seizure start date is required!");
+            return;
+        }
+
+        if (seizureFreq == "0") {
+            openDatePicker.requestFocus();
+            openDatePicker.setError("A seizure start date is required!");
+            return;
+        }
+
+        if (averageSeizure == "0") {
             openDatePicker.requestFocus();
             openDatePicker.setError("A seizure start date is required!");
             return;
@@ -203,7 +199,7 @@ public class QuestionnaireMedical extends AppCompatActivity implements View.OnCl
         questionnaireComplete("questionnaire bool", "1");
 
         // Moving to Datatable fragment
-        startActivity(new Intent(this, Navbar.class));
+        startActivity(new Intent(this, LocationPermission.class));
         
     }
 
